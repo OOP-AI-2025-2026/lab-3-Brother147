dd26936396d6976a7d3a1fa4fde1db862fbbf483
Brother147 <105119480+Brother147@users.noreply.github.com>
2025-12-04 20:19:03 +0200

    Update Item.java

--- src/main/java/org/example/task2/Item.java
+++ src/main/java/org/example/task2/Item.java
@@ -1,15 +1,31 @@
 package org.example.task2;
 
+/**
+ * Клас товару.
+ * Інкапсульовані поля з публічними геттерами.
+ */
 public class Item {
 
-    public long id;
-    public String name;
-    public double price;
+    private long id;
+    private String name;
+    private double price;
 
-    public Item(long _id, String _name, double _price) {
-        this.id = _id;
-        this.name = _name;
-        this.price = _price;
+    public Item(long id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
     }
 
     @Override
